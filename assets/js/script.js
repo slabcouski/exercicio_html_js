61782ca1a82a442c40183e27198b6daf32a1f003
Marcelo Garcia <prof.marcelo.garcia@gmail.com>
2023-05-11 13:33:20 -0300

    11/05/2023 -13:33

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,17 @@ let valueA = document.getElementById('numberA.value');
 let valueB = document.getElementById('numberB.value');
 
 console.log(valueA, valueB)
+
+//function validarFormulario(event) {
+    //event.preventDefault(); // previne o envio do formulário
+    
+    //var campoA = document.getElementById("campoA").value;
+    //var campoB = document.getElementById("campoB").value;
+    
+    //if (campoB <= campoA) {
+      //alert("O número B deve ser maior que o número A!");
+      //return false;
+    //}
+    
+    //document.getElementById("meuFormulario").submit(); // envia o formulário
+  //}
