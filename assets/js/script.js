9ec38feee8356c412e713a061e4fccfdb7a76381
slabcouski <prof.marcelo.garcia@gmail.com>
2023-05-12 19:21:10 -0300

    12/05/2023 -19:21

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,26 @@
-let valueA = document.getElementById('numberA.value');
-let valueB = document.getElementById('numberB.value');
+function validaForm(event){
+  event.preventDefault()
+    const valueA = document.getElementById('numberA').value
+    const valueB = document.getElementById('numberB').value
+    console.log(valueA, valueB)
 
-console.log(valueA, valueB)
+      function calculaValor(valueA, valueB){
+        return valueA * valueB
+      }
+      if (valueB <= valueA){
+        alert("O número B deve ser maior que o número A!");
+      } else {
+        let valorTotal = calculaValor(valueA, valueB);
+        alert('O valor do calculo é: ' + valorTotal);
+      }
+}
+
+
+  //return valueA * valueB
+//}
+//console.log(valorTotal);
+
+//document.getElementById('btn-calc').submit;
 
 //function validarFormulario(event) {
     //event.preventDefault(); // previne o envio do formulário
